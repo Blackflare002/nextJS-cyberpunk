10a1f3caff1ee9c60d37d52092a78725f6fe8a44
Shane Phillips <shane@loongphillips.com>
2023-06-05 16:21:32 -0400

    fonts added, homepage styling, list item styling

--- app/components/Items.jsx
+++ app/components/Items.jsx
@@ -1,23 +1,48 @@
 import styles from "./Items.module.css";
+import Link from "next/link";
 
 const Items = ({ items }) => {
 	return (
 		<div>
 			<li className={styles.styledLi}>
-				<div>
-					<img
-						className={styles.styledImg}
-						src={items.imageSrc}
-					/>
-					<p>
-						{items.name.length < 65
-							? items.name
-							: items.name.slice(0, 65) + "..."}
-					</p>
-				</div>
-				<div>
-					<p>In Stock: {items.numInStock}</p>
-					<p>{items.price}</p>
+				<div className={styles.itemContainer}>
+					<Link href=""></Link>
+					<div className={styles.itemInnerBox}>
+						<div>
+							<img
+								className={styles.styledImg}
+								src={items.imageSrc}
+							/>
+							<p
+								className={styles.styledItemName}
+							>
+								{items.name.length < 65
+									? items.name
+									: items.name.slice(0, 65) +
+									  "..."}
+							</p>
+						</div>
+						<div>
+							<p>In Stock: {items.numInStock}</p>
+						</div>
+						{items.numInStock === 0 && (
+							<div className={styles.OOSRibbon}>
+								Sold Out
+							</div>
+						)}
+						<div className={styles.priceAndBtn}>
+							<p
+								className={styles.styledItemPrice}
+							>
+								{items.price}
+							</p>
+							<button
+								className={styles.styledButton}
+							>
+								Add to Cart
+							</button>
+						</div>
+					</div>
 				</div>
 			</li>
 		</div>
--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 "use client";
 import { useState, useEffect } from "react";
 import Items from "./components/Items";
+import styles from "./page.module.css";
+import "./styles/fonts.css";
 
 const Homepage = () => {
 	const [items, setItems] = useState([]);
@@ -18,18 +20,35 @@ const Homepage = () => {
 
 	return (
 		<>
-			<ul>
+			<div className={styles.hero}>
 				<div>
-					{items.map((item) => (
-						<Items
-							key={Math.floor(
-								Math.random() * 4206969
-							)}
-							items={item}
-						/>
-					))}
+					<h1
+						className={styles.bigTypography}
+						style={{ fontFamily: "TronFont" }}
+					>
+						HACKERMAN
+					</h1>
 				</div>
-			</ul>
+				<div>
+					<h1 className={styles.tagline}>
+						Premium Cyber Gear
+					</h1>
+				</div>
+			</div>
+			<div>
+				<ul className={styles.styledUl}>
+					<div className={styles.wrapper}>
+						{items.map((item) => (
+							<Items
+								key={Math.floor(
+									Math.random() * 4206969
+								)}
+								items={item}
+							/>
+						))}
+					</div>
+				</ul>
+			</div>
 		</>
 	);
 };
