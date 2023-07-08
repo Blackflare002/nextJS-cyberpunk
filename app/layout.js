54f2df475ad4035d8a58f60852566b020c433105
Shane Phillips <shane@loongphillips.com>
2023-07-08 17:12:49 -0400

    OGL BS attempt 1

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import Header from "./components/Header";
 import { AppContextProvider } from "./components/AppContext";
 import { CartProvider } from "./components/CartContext";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,6 +11,20 @@ export const metadata = {
 	title: "Hackerman Premium Cyber Gear",
 	description:
 		"Cutting edge tech, cutting edge prices!",
+	openGraph: {
+		title: "Hackerman",
+		description: "A Cyberpunk E-Commerce Website",
+		siteName: "Hackerman",
+		images: [
+			{
+				url: "./opengraph-image.png",
+				width: 800,
+				height: 600,
+			},
+		],
+		locale: "en_US",
+		type: "website",
+	},
 };
 
 export default function RootLayout({ children }) {
@@ -18,6 +33,57 @@ export default function RootLayout({ children }) {
 			<AppContextProvider>
 				<CartProvider>
 					<body className={inter.className}>
+						<Head>
+							<meta
+								property="title"
+								content="Hackerman"
+							/>
+							<meta
+								property="og:title"
+								content="Hackerman"
+							/>
+							<meta
+								property="description"
+								content="Cutting edge tech, cutting edge prices!"
+							/>
+							<meta
+								property="og:description"
+								content="Cutting edge tech, cutting edge prices!"
+							/>
+							<meta
+								property="og:url"
+								content="https://next-js-13-blue.vercel.app/"
+							/>
+							<meta
+								property="og:site_name"
+								content="Hackerman"
+							/>
+							<meta
+								property="og:locale"
+								content="en_US"
+							/>
+							<meta
+								property="og:image"
+								content="<generated>"
+							/>
+							<meta
+								property="og:image:type"
+								content="<generated>"
+							/>
+							<meta
+								property="og:image:width"
+								content="<generated>"
+							/>
+							<meta
+								property="og:image:height"
+								content="<generated>"
+							/>
+							<meta
+								property="og:type"
+								content="website"
+							/>
+						</Head>
+
 						<Header />
 						{children}
 					</body>
